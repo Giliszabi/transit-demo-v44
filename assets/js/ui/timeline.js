82ba3e4ed28a6e54d9f6bbb84200107f1f235cbb
Giliszabi <87413536+Giliszabi@users.noreply.github.com>
2026-03-17 11:17:59 +0100

    Add renderTimeline function to display timeline

--- assets/js/ui/timeline.js
+++ assets/js/ui/timeline.js
@@ -1,1 +1,16 @@
+export function renderTimeline(containerId, eroforrasLista) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
 
+  eroforrasLista.forEach((eroforras) => {
+    const div = document.createElement("div");
+    div.className = "timeline-resource";
+    div.innerHTML = `
+      <div class="timeline-resource-name">
+         ${eroforras.icon} ${eroforras.name}
+      </div>
+      <div class="timeline-bar"></div>
+    `;
+    container.appendChild(div);
+  });
+}
