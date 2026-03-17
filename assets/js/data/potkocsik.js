61742021c61ce30e938174dc51325041fabac7db
Giliszabi <87413536+Giliszabi@users.noreply.github.com>
2026-03-17 12:16:24 +0100

    Add initial data for POTKOCSIK array

--- assets/js/data/potkocsik.js
+++ assets/js/data/potkocsik.js
@@ -1,1 +1,14 @@
-export const POTKOCSIK = [];
+export const POTKOCSIK = [
+  {
+    id: "P1",
+    rendszam: "POT-111",
+    jelenlegi_pozicio: { hely: "Budapest" },
+    timeline: []
+  },
+  {
+    id: "P2",
+    rendszam: "POT-222",
+    jelenlegi_pozicio: { hely: "Miskolc" },
+    timeline: []
+  }
+];
