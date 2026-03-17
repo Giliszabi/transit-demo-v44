9094138d094be96156b059c0492f0248c383d538
Giliszabi <87413536+Giliszabi@users.noreply.github.com>
2026-03-17 12:15:52 +0100

    Add sample driver data to SOFOROK array

--- assets/js/data/soforok.js
+++ assets/js/data/soforok.js
@@ -1,1 +1,29 @@
-export const SOFOROK = [];
+export const SOFOROK = [
+  {
+    id: "S1",
+    nev: "Teszt Sofőr 1",
+    jelenlegi_pozicio: { hely: "Budapest" },
+    timeline: [
+      {
+        start: "2026-03-17T08:00",
+        end: "2026-03-17T12:00",
+        type: "piheno",
+        label: "Pihenő"
+      }
+    ]
+  },
+  {
+    id: "S2",
+    nev: "Teszt Sofőr 2",
+    jelenlegi_pozicio: { hely: "Győr" },
+    timeline: [
+      {
+        start: "2026-03-17T06:00",
+        end: "2026-03-17T15:00",
+        type: "fuvar",
+        label: "Aktív fuvar"
+      }
+    ]
+  }
+];
+``
