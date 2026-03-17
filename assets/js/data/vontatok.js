e374c455824492dd09e1aa6492f3cb8616f78335
Giliszabi <87413536+Giliszabi@users.noreply.github.com>
2026-03-17 12:16:08 +0100

    Add initial VONTATOK data structure

--- assets/js/data/vontatok.js
+++ assets/js/data/vontatok.js
@@ -1,1 +1,28 @@
-export const VONTATOK = [];
+export const VONTATOK = [
+  {
+    id: "V1",
+    rendszam: "ABC-123",
+    jelenlegi_pozicio: { hely: "Budapest" },
+    timeline: [
+      {
+        start: "2026-03-17T10:00",
+        end: "2026-03-17T18:00",
+        type: "fuvar",
+        label: "Szállítás"
+      }
+    ]
+  },
+  {
+    id: "V2",
+    rendszam: "XYZ-555",
+    jelenlegi_pozicio: { hely: "Debrecen" },
+    timeline: [
+      {
+        start: "2026-03-17T05:00",
+        end: "2026-03-17T20:00",
+        type: "szerviz",
+        label: "Szerviz"
+      }
+    ]
+  }
+];
